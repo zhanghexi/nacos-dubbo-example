ee946e756a2eee5bcf03b1ab43a130d6dedae831
zhang <zhanghx6@asiainfo.com>
2020-10-20 17:35:37 +0800

    修改内部实现方法的写法

--- dubbo-provider/src/main/java/org/example/provider/rpc/RpcRemoteProviderImpl.java
+++ dubbo-provider/src/main/java/org/example/provider/rpc/RpcRemoteProviderImpl.java
@@ -3,16 +3,18 @@ package org.example.provider.rpc;
 import lombok.extern.log4j.Log4j2;
 import org.apache.dubbo.config.annotation.Service;
 import org.example.api.service.RpcRemoteProvider;
+import org.springframework.stereotype.Component;
 
 /**
  * @ClassName RpcRemoteProviderImpl
  * @User zhang
- * @Description
+ * @Description 为了方便服务内部引用，要加上@Component注解
  * @Author Lucien
  * @Date 2020/10/20 14:18
  * @Version 1.0
  */
 @Log4j2
+@Component
 @Service(version = "1.0.0")
 public class RpcRemoteProviderImpl implements RpcRemoteProvider {
 
